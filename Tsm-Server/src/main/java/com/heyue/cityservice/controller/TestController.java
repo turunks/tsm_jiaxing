05f1123447b15c1ef7bf081eb28d2b7fd44cb1a0
turunks <482552988@qq.com>
2021-11-13 23:54:04 +0800

    读写文件test

--- Tsm-Server/src/main/java/com/heyue/cityservice/controller/TestController.java
+++ Tsm-Server/src/main/java/com/heyue/cityservice/controller/TestController.java
@@ -3,10 +3,16 @@ package com.heyue.cityservice.controller;
 
 import cn.com.heyue.entity.TsmTerminalOrder;
 import cn.com.heyue.mapper.TsmTerminalOrderMapper;
+import cn.com.heyue.utils.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 
 /**
@@ -27,4 +33,32 @@ public class TestController {
         return tsmTerminalOrder;
     }
 
+    @RequestMapping("writeFile")
+    @ResponseBody
+    public void createCard() {
+        SimpleDateFormat df = new SimpleDateFormat("yyMMddHHmmss");
+        String date_2 = df.format(new Date().getTime());
+        String path = "D:\\" + date_2 + ".SQ";
+        List<String> accountList = new ArrayList<>();
+        accountList.add("111,222,333");
+        accountList.add("TRADELIST END,000000,000000000000");
+        try {
+            FileUtils.writeFileContext(accountList, path);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @RequestMapping("readFile")
+    @ResponseBody
+    public void readFile () {
+        try {
+            String path = "D:\\" + "211113233614" + ".SQ";
+            List<String> list = FileUtils.getFile(path);
+            String s = list.toString();
+            System.out.println(s);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
